29549d5da1a664be3fcac3fdcb224d115051185b
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2024-04-11 17:09:24 +0800

    week08

---
+++ week08-1_again/main.cpp
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+///這裡等一下要插入 3段 程式碼
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/Gundam.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel(); ///glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+int main(int argc, char*argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week08 Gundam");
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}

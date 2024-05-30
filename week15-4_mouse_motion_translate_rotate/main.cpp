9867c604769769b9b0ed7ea2dece62589d36d3d6
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2024-05-30 17:53:16 +0800

    week15

--- week15-4_mouse_motion_translate_rotate/main.cpp
+++ week15-4_mouse_motion_translate_rotate/main.cpp
@@ -9,6 +9,7 @@ void display() {
 
     glPushMatrix();
         ///glTranslatef(teapotX, teapotY, 0);
+        glTranslatef(0, 0, +6);
         glRotatef(angleX[0], 0, 0, 1);
         glTranslatef(-0.520, -0.133, 0);
         glutSolidTeapot( 0.3 );
@@ -29,14 +30,28 @@ void motion(int x, int y) {
     oldY = y;
     glutPostRedisplay();
 }
+static void resize(int width, int height)
+{
+    const float ar = (float) width / (float) height;
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-ar, ar, -1.0, 1.0, 0.01, 10.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
 int main(int argc, char**argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutInitWindowSize(800,400);
     glutCreateWindow("week15-4");
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
     glutMotionFunc(motion);
+    glutReshapeFunc(resize);
 
     glutMainLoop();
 }
